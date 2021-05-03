fc36511c277adeb814b9e2b71f3de7dc49365858
Shay Keinan <crazypixel@gmail.com>
2021-05-03 15:00:19 +0300

    first components using create element

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement('div', null, [
+    React.createElement('h1', null, 'My Groceries'),
+  
+    React.createElement('ul', null, [
+      React.createElement('li', null, 'Milk'),
+      React.createElement('li', null, 'Eggs'),
+      React.createElement('li', null, 'Bread'),
+      React.createElement('li', null, 'Apples'),
+    ])
+  ]),
   document.getElementById('root')
 );
 
